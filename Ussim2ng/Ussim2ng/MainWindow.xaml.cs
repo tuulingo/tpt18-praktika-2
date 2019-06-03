1756235c46f7d6f14ac0adbe0acb5ed8a055be21
TomAbel <33309474+TomAbel@users.noreply.github.com>
2019-06-03 14:04:22 +0300

    Movement

--- Ussim2ng/Ussim2ng/MainWindow.xaml.cs
+++ Ussim2ng/Ussim2ng/MainWindow.xaml.cs
@@ -24,5 +24,52 @@ namespace Ussim2ng
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                double currentLeft = Canvas.GetLeft(rectangle1);
+                double newleft = currentLeft + 20;
+                Canvas.SetLeft(rectangle1, newleft);
+            }
+
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                Canvas.SetLeft(rectangle1, 20);
+            }
+
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Right)
+            {
+                double currentLeft = Canvas.GetLeft(rectangle1);
+                double newleft = currentLeft + 20;
+                Canvas.SetLeft(rectangle1, newleft);
+            }
+
+            if (e.Key == Key.Down)
+            {
+                double currentDown = Canvas.GetTop(rectangle1);
+                double newDown = currentDown + 20;
+                Canvas.SetTop(rectangle1, newDown);
+            }
+
+            if (e.Key == Key.Left)
+            {
+                double currentLeft = Canvas.GetLeft(rectangle1);
+                double newleft = currentLeft - 20;
+                Canvas.SetLeft(rectangle1, newleft);
+            }
+
+            if (e.Key == Key.Up)
+            {
+                double currentLeft = Canvas.GetTop(rectangle1);
+                double newleft = currentLeft - 20;
+                Canvas.SetTop(rectangle1, newleft);
+            }
+        }
     }
 }
