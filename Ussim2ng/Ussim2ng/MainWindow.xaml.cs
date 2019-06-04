4a1faac2d2ea140e5227f5099aecebef3074381d
TomAbel <33309474+TomAbel@users.noreply.github.com>
2019-06-04 11:45:34 +0300

    Grid

--- Ussim2ng/Ussim2ng/MainWindow.xaml.cs
+++ Ussim2ng/Ussim2ng/MainWindow.xaml.cs
@@ -20,54 +20,66 @@ namespace Ussim2ng
     /// </summary>
     public partial class MainWindow : Window
     {
+        const double CellSize = 30;
+        const int Cellcount = 16;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            test();
         }
 
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        public void test()
         {
-            if(e.LeftButton == MouseButtonState.Pressed)
-            {
-                double currentLeft = Canvas.GetLeft(rectangle1);
-                double newleft = currentLeft + 20;
-                Canvas.SetLeft(rectangle1, newleft);
-            }
 
-            if (e.RightButton == MouseButtonState.Pressed)
+            for (int y = 0; y < Cellcount; y++)
             {
-                Canvas.SetLeft(rectangle1, 20);
-            }
+                for (int x = 0; x < Cellcount; x++)
+                {
+                    if (x % 2 == 0 && y % 2 == 0 || x % 2 == 1 && y % 2 == 1)
+                    { 
+                        Rectangle r1 = new Rectangle();
+                        r1.Height = CellSize;
+                        r1.Width = CellSize;
+                        r1.Fill = Brushes.LimeGreen;
+                        Canvas.SetTop(r1, x * CellSize);
+                        Canvas.SetLeft(r1, y * CellSize);
+                        board.Children.Add(r1);
+                    }
 
+                }
+
+            }
         }
 
-        private void Window_KeyDown(object sender, KeyEventArgs e)
+        private void Window_KeyDown_1(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Right)
             {
                 double currentLeft = Canvas.GetLeft(rectangle1);
-                double newleft = currentLeft + 20;
+                double newleft = currentLeft + 30;
                 Canvas.SetLeft(rectangle1, newleft);
             }
 
             if (e.Key == Key.Down)
             {
                 double currentDown = Canvas.GetTop(rectangle1);
-                double newDown = currentDown + 20;
+                double newDown = currentDown + 30;
                 Canvas.SetTop(rectangle1, newDown);
             }
 
             if (e.Key == Key.Left)
             {
                 double currentLeft = Canvas.GetLeft(rectangle1);
-                double newleft = currentLeft - 20;
+                double newleft = currentLeft - 30;
                 Canvas.SetLeft(rectangle1, newleft);
             }
 
             if (e.Key == Key.Up)
             {
                 double currentLeft = Canvas.GetTop(rectangle1);
-                double newleft = currentLeft - 20;
+                double newleft = currentLeft - 30;
                 Canvas.SetTop(rectangle1, newleft);
             }
         }
